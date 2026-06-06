887fef743041eb9823bf1cc7b29c54da29419088
Vupi <276366791+FMD911@users.noreply.github.com>
2026-06-06 09:55:11 +0200

    implement linked list append method

---
+++ src/LinkedList.js
@@ -0,0 +1,27 @@
+import Node from "./Node.js";
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+    }
+
+    current.nextNode = newNode;
+  }
+}
+
+export default LinkedList;
+
